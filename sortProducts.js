e9a664cf53eb9f95dd30e68b5852f6fd1fe3d64f
Remy <shingiroremy303@gmail.com>
2025-08-19 16:27:39 +0200

    implement sortProducts function stub for sorting products by price

---
+++ sortProducts.js
@@ -0,0 +1,9 @@
+// You are given an array of product objects, 
+// each containing a name (string) and a price 
+// (number). Your task is to implement a function 
+// called sortProducts that sorts the products based on their
+//  prices in ascending order. 
+
+const sortProducts = () => {
+    
+}
